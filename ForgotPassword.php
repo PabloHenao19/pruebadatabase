d2fe1a15adad0079571c8700b1007aa561510f4f
PabloHenao19 <henaopablo16@gmail.com>
2023-06-11 03:56:06 -0500

    segundo commit

--- ForgotPassword.php
+++ ForgotPassword.php
@@ -32,36 +32,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $query->bindParam(':user_id', $user['id']);
       $query->execute();
 
-      // Enviar correo electrónico al usuario con el enlace para restablecer la contraseña
+      // Construir el enlace para restablecer la contraseña
       $resetLink = "https://example.com/reset_password.php?token=$token";
 
-      // Configurar el correo electrónico
-      $mail = new PHPMailer();
-      $mail->isSMTP();
-      $mail->Host = 'smtp.example.com'; // Configura el servidor SMTP que utilizarás
-      $mail->SMTPAuth = true;
-      $mail->Username = 'tu_correo@example.com'; // Coloca aquí tu dirección de correo electrónico
-      $mail->Password = 'tu_contraseña'; // Coloca aquí tu contraseña de correo electrónico
-      $mail->SMTPSecure = 'tls';
-      $mail->Port = 587;
-
-      $mail->setFrom('tu_correo@example.com', 'Tu Nombre'); // Configura la dirección y el nombre del remitente
-      $mail->addAddress($email); // Agrega la dirección de correo electrónico del destinatario
-
-      $mail->isHTML(true);
-      $mail->Subject = 'Restablecimiento de contraseña';
-      $mail->Body = "Haz clic en el siguiente enlace para restablecer tu contraseña: <a href='$resetLink'>$resetLink</a>";
-
-      // Envía el correo electrónico
-      if ($mail->send()) {
-        $_SESSION['message'] = 'Se ha enviado un enlace de restablecimiento de contraseña a tu correo electrónico.';
-        header('Location: ForgotPassword.php');
-        exit();
-      } else {
-        $_SESSION['error'] = 'Ocurrió un error al enviar el correo electrónico. Por favor, inténtalo de nuevo más tarde.';
-        header('Location: ForgotPassword.php');
-        exit();
-      }
+      // Redireccionar a la página de restablecimiento de contraseña
+      $_SESSION['reset_link'] = $resetLink;
+      header('Location: reset_password.php');
+      exit();
     } else {
       // Código a ejecutar si no se encontró ningún usuario con el correo electrónico proporcionado
       $_SESSION['error'] = 'No se encontró ningún usuario con el correo electrónico proporcionado.';
--- ResetPassword.php
+++ ResetPassword.php
@@ -1,11 +1,11 @@
 <?php
-session_start();
-require 'database.php';
+session_start(); // Iniciar la sesión para utilizar variables de sesión
+require 'database.php'; // Requerir el archivo de conexión a la base de datos
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $token = $_POST['token'];
-  $password = $_POST['password'];
-  $confirmPassword = $_POST['confirm_password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { // Verificar si se envió el formulario por el método POST
+  $token = $_POST['token']; // Obtener el token enviado en el formulario
+  $password = $_POST['password']; // Obtener la contraseña ingresada en el formulario
+  $confirmPassword = $_POST['confirm_password']; // Obtener la confirmación de contraseña ingresada en el formulario
 
   // Verificar si el token es válido y aún no ha caducado
   $query = $conn->prepare('SELECT id FROM users WHERE reset_token = :token AND reset_token_expires > NOW()');
@@ -13,20 +13,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $query->execute();
   $user = $query->fetch(PDO::FETCH_ASSOC);
 
-  if ($user && $password === $confirmPassword) {
+  if ($user && $password === $confirmPassword) { // Verificar si el usuario existe y las contraseñas coinciden
     // Actualizar la contraseña en la base de datos y eliminar el token
-    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // Hashear la contraseña antes de almacenarla en la base de datos
     $query = $conn->prepare('UPDATE users SET password = :password, reset_token = NULL, reset_token_expires = NULL WHERE id = :user_id');
     $query->bindParam(':password', $hashedPassword);
     $query->bindParam(':user_id', $user['id']);
     $query->execute();
 
-    $_SESSION['message'] = 'Contraseña actualizada correctamente.';
-    header('Location: login.php');
+    $_SESSION['message'] = 'Contraseña actualizada correctamente.'; // Establecer un mensaje de éxito en la sesión
+    header('Location: login.php'); // Redireccionar al usuario a la página de inicio de sesión
     exit();
   } else {
-    $_SESSION['error'] = 'El token no es válido o la contraseña no coincide.';
-    header('Location: reset_password.php?token=' . $token);
+    $_SESSION['error'] = 'El token no es válido '; // Establecer un mensaje de error en la sesión
+    header('Location: ResetPassword.php?token=' . $token); // Redireccionar al usuario de vuelta al formulario de restablecimiento de contraseña
     exit();
   }
 }
@@ -39,21 +39,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <?php if (isset($_SESSION['error'])): ?>
-    <p>Error: <?php echo $_SESSION['error']; ?></p>
-    <?php unset($_SESSION['error']); ?>
+  <?php if (isset($_SESSION['error'])): ?> <!--Verificar si hay un mensaje de error en la sesión-->
+    <script>alert("Error: <?php echo $_SESSION['error']; ?>");</script> <!-- Mostrar el mensaje de error mediante una alerta de JavaScript -->
+    <?php unset($_SESSION['error']); ?> <!-- Eliminar el mensaje de error de la sesión -->
   <?php endif; ?>
 
   <h2>Reset Password</h2>
   <form method="post">
-    <input type="hidden" name="token" value="<?php echo $_GET['token']; ?>">
-    <label for="password">Nueva contraseña:</label>
-    <input type="password" name="password" id="password" required>
+    <input type="hidden" name="token" value="<?php echo isset($_GET['token']) ? $_GET['token'] : ''; ?>"> 
+    <label for="password">Nueva contraseña:</label> 
+    <input type="password" name="password" id="password" required> 
     <br>
-    <label for="confirm_password">Confirmar contraseña:</label>
-    <input type="password" name="confirm_password" id="confirm_password" required>
+    <label for="confirm_password">Confirmar contraseña:</label> 
+    <input type="password" name="confirm_password" id="confirm_password" required> 
+    <?php if (isset($_SESSION['error']) && $_SESSION['error'] === 'Contraseña no coincide.'): ?> 
+      <script>alert("Contraseña no coincide.");</script> <!-- Mostrar la alerta de error específica mediante una alerta de JavaScript -->
+    <?php endif; ?>
     <br>
-    <input type="submit" value="Restablecer contraseña">
+    <input type="submit" value="Restablecer contraseña"> 
   </form>
 </body>
 </html>
